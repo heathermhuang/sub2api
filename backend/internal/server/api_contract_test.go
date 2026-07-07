6d68f53f0e68be1ff1c0be0eae5401876980e184
Heatherm Huang <heathermhuang@gmail.com>
2026-07-07 21:10:15 +0800

    test: update settings contracts for custom domains

--- backend/internal/server/api_contract_test.go
+++ backend/internal/server/api_contract_test.go
@@ -904,6 +904,7 @@ func TestAPIContracts(t *testing.T) {
 						"rules": []
 					},
 					"custom_menu_items": [],
+					"custom_domains_enabled": false,
 					"custom_endpoints": [],
 					"payment_enabled": false,
 					"payment_min_amount": 0,
@@ -1103,6 +1104,7 @@ func TestAPIContracts(t *testing.T) {
 					"auth_source_default_wechat_platform_quotas": null,
 					"auth_source_default_dingtalk_platform_quotas": null,
 					"custom_menu_items": [],
+					"custom_domains_enabled": false,
 					"custom_endpoints": [],
 					"default_concurrency": 0,
 					"default_balance": 0,
